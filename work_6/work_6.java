3d1e16808a4cc75f3bc4990793b7900a11db5da7
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2020-09-20 19:14:02 +0700

    Update work_6.java

--- work_6/work_6.java
+++ work_6/work_6.java
@@ -1,5 +1,4 @@
 import java.io.*;
-
 class aa{
     static String data[][] = new String[3][3];
         public static void aaa() throws IOException{
@@ -25,8 +24,6 @@ class aa{
             } 
         }        
 }
-
-
 class bb extends aa{
     public static void bbb() {
         try {
@@ -51,8 +48,7 @@ class bb extends aa{
                 }
                 if(i == 2){
                     sum +=Integer.parseInt(data[2][j]);
-                }
-                
+                }           
             }
             System.out.println("|");
         }
@@ -62,14 +58,10 @@ class bb extends aa{
         System.out.println("tax 7% = " + (sum * 0.07)+ " $");
         System.out.println("Net salary = " + ((sum*0.93)-500) + " $");
         System.out.println("-------------------------------------");
-       //System.out.print("total = " + String.parseString(sum));
-        } catch (Exception e) {
-            
-        }
-        
+        } catch (Exception e) {           
+        }     
     }
 }
-
 class cc extends bb {
     public static void main(String args[]) {
 
