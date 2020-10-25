bca18384b071c6968526f192dfc74befbad8a975
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2020-10-25 14:07:41 +0700

    Add files via upload

---
+++ work_9/zipcode_2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class x {
+    public static void main(String args [])throws IOException {
+        System.out.print("Input zipcode : ");
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        String getzipcode = stdin.readLine();
+        int i= 1;
+        String b;
+        String[] fields;
+        String patternStr = ",";
+        FileReader fin = new FileReader("zipcode1.txt");
+        BufferedReader bin = new BufferedReader (fin);
+        // System.out.println(b = bin.readLine()); 
+        while ((b = bin.readLine()) != null) {
+            fields = b.split(patternStr);
+            if(fields[1].equals(getzipcode)){
+                System.out.println("latitude : " + fields[4]);
+                System.out.println("longitude : " + fields[5]);
+            }
+            i=i+1;
+        }
+        fin.close();
+    }
+}
