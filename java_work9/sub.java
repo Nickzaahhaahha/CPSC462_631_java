7be19acbea165f0547b63257f5d1559dbfdff2b4
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2020-11-01 23:31:35 +0700

    Add files via upload

---
+++ java_work9/sub.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.lang.*;
+class x {
+public static void main (String args[]) throws IOException {
+int i = 0;
+String b,f,h;
+String[] fields;
+String patternStr = ",";
+FileReader finF = new FileReader("f.txt");
+BufferedReader binF = new BufferedReader (finF);
+
+FileReader finH = new FileReader("h.txt");
+BufferedReader binH = new BufferedReader (finH);
+
+FileReader fin = new FileReader("sub.txt");
+BufferedReader bin = new BufferedReader (fin);
+
+FileOutputStream fout = new FileOutputStream("sub.htm");
+BufferedOutputStream bout = new BufferedOutputStream(fout);
+PrintStream pout = new PrintStream(bout);
+
+while ((f =  binH.readLine()) != null) {pout.println(f);}
+pout.println("<body bgcolor=yellow><table border=1 width=100%>");
+while ((b =  bin.readLine()) != null) {
+fields = b.split(patternStr);
+pout.println("<tr>");
+pout.println("<td>"+ fields[0]+"</td>");
+pout.println("<td>"+fields[1]+"</td>");
+pout.println("</tr>");
+i = i + 1;
+}
+pout.println("</table></body>");
+while ((h =  binF.readLine()) != null) {pout.println(h);}
+fin.close();
+pout.close();
+}
+}
